15d938ff11d571fe532114ac83966c37b496a42d
Oleg <saharuk_totti10@bk.ru>
2018-12-01 15:24:33 +0200

    Update Program.java

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,28 +1,28 @@
 public class Program {
     public static void main(String[] args) {
-        char[] arr = "titani".toCharArray();
-        for (int i = 0; i < (arr.length % 2 == 1 ? arr.length : arr.length - 1); i++) { 
+        char[] arr = "titaniksq".toCharArray();
+        for (int i = 0; i < (arr.length%2==1?arr.length:arr.length-1); i++) {
             for (int j = 0; j < arr.length; j++) {
-                if (i <= (arr.length % 2 == 1 ? arr.length / 2 : arr.length / 2 - 1)) {
-                    if ((j < arr.length / 2 - ((arr.length % 2 == 1) ? i : i + 1) || (j > arr.length / 2 + i))) {
-                        System.out.print(" ");
-                    } else {
-                        System.out.print(arr[j]);
-                    }
-                } else {
-                    if ((j <= -(arr.length / 2 - (arr.length % 2 == 0 ? i : i - 1))
-                            || (j >= arr.length / 2 + (arr.length - (arr.length % 2 == 0 ? i + 1 : i))))) {
+                if(i<=(arr.length%2==1?arr.length/2:arr.length/2-1)){
+                    if ((j < arr.length / 2 - ((arr.length%2==1)?i:i+1) || (j > arr.length / 2 + i))){
                         System.out.print(" ");
                     } else {
                         System.out.print(arr[j]);
                     }
                 }
-                System.out.println();
+                else{
+                    if ((j <= -(arr.length / 2 - (arr.length%2==0?i:i-1))
+                            ||(j>=arr.length/2+(arr.length-(arr.length%2==0?i+1:i))))) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print(arr[j]);
+                    }
+                }
             }
+            System.out.println();
         }
     }
 }
-
 
 
 
