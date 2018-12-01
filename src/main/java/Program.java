ff9d9b45502da1b4eb9340a6e8929c8ed131cd56
Oleg Sakharuk <saharuk_totti10@bk.ru>
2018-12-01 14:54:37 +0200

    ready task

---
+++ src/main/java/Program.java
@@ -0,0 +1,29 @@
+public class Program {
+    public static void main(String[] args) {
+        char[] arr = "titani".toCharArray();
+        for (int i = 0; i < (arr.length % 2 == 1 ? arr.length : arr.length - 1); i++) { // якщо довжина слова не парна тоді
+            for (int j = 0; j < arr.length; j++) {
+                if (i <= (arr.length % 2 == 1 ? arr.length / 2 : arr.length / 2 - 1)) {
+                    if ((j < arr.length / 2 - ((arr.length % 2 == 1) ? i : i + 1) || (j > arr.length / 2 + i))) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print(arr[j]);
+                    }
+                } else {
+                    if ((j <= -(arr.length / 2 - (arr.length % 2 == 0 ? i : i - 1))
+                            || (j >= arr.length / 2 + (arr.length - (arr.length % 2 == 0 ? i + 1 : i))))) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print(arr[j]);
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+
+
+
+
