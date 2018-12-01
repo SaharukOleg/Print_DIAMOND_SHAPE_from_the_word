8fb57d960011cbc3166a9bf95936628ca10dd00f
Oleg <saharuk_totti10@bk.ru>
2018-12-01 14:56:21 +0200

    Update Program.java

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,7 +1,7 @@
 public class Program {
     public static void main(String[] args) {
         char[] arr = "titani".toCharArray();
-        for (int i = 0; i < (arr.length % 2 == 1 ? arr.length : arr.length - 1); i++) { // якщо довжина слова не парна тоді
+        for (int i = 0; i < (arr.length % 2 == 1 ? arr.length : arr.length - 1); i++) { 
             for (int j = 0; j < arr.length; j++) {
                 if (i <= (arr.length % 2 == 1 ? arr.length / 2 : arr.length / 2 - 1)) {
                     if ((j < arr.length / 2 - ((arr.length % 2 == 1) ? i : i + 1) || (j > arr.length / 2 + i))) {
